3938a5f6d70ff260b03758e6ef3cb522d8ca6380
Adityarj6623 <157475751+Adityarj6623@users.noreply.github.com>
2024-10-16 10:42:50 +0000

    container

---
+++ 01_basics/03_dt_conversion.js
@@ -0,0 +1,21 @@
+let score="hd"
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+
+//"33" =>33
+//"33and" => NAN
+// true => 1;false=0;
+ 
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1=>TRUE 0=>FALSE 
