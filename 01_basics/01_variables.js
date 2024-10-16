2d07fb42ec164962c33cf91b7ea2e37592ca3ec5
Adityarj6623 <157475751+Adityarj6623@users.noreply.github.com>
2024-10-16 09:37:57 +0000

    container

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "adityarajbs6623@gmail.com"
+var accountPassword = "12345"
+accountCity = "gaya"
+let accountstate;
+// accountId=32458 not allowed
+//var ke andr scope wala problem hai  block scope and functional svope
+
+//use const or let
+
+
+accountEmail="hc@hc.com"
+accountPassword="9785893"
+accountCity="infis"
+
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountstate,accountPassword,accountCity])
