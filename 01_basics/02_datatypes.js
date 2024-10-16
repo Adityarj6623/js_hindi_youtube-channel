4d57afb0c2b230e4a1979747b13ad04f920b941a
Adityarj6623 <157475751+Adityarj6623@users.noreply.github.com>
2024-10-16 10:19:25 +0000

    container

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as newer version
+
+// alert(2+2) 
+// we are using nodejs,not browser
+//code readability should be high
+
+console.log(3+3)
+
+ console.log("aditya")
+
+let name="aditya"
+let age=18
+let isloggedin=false
+let state;
+
+//number= 2 to the power of 53
+//bigint
+// string =" "
+// boolean = true /false
+// null = standalone value
+// undefined = abhi value asign nhi hui hai
+// symbol = use for uniqueness
+
+
+
+//object
+
+console.log(typeof undefined)   //undefined
+console.log(typeof null)  //object
