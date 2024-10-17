d75a2fd27f022b7cda6aff9f9c01737379c3aba4
Adityarj6623 <157475751+Adityarj6623@users.noreply.github.com>
2024-10-17 06:24:22 +0000

    container

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2==1)
+// console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+
+
+
+
+//es trh ka conversion hmesha confusion me hi dalte hai
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+
+
+//streak check === it check value with its datatype
+console.log("2"===2)
