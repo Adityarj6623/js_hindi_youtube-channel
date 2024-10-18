a205464d12526dea92d194f5c4138f697f051306
Adityarj6623 <157475751+Adityarj6623@users.noreply.github.com>
2024-10-18 09:40:38 +0000

    container

---
+++ 01_basics/06_summary.js
@@ -0,0 +1,46 @@
+//interview perspective 
+
+ //primitive and non-primitive datatype<--  kis trh se data ko memory me rkha jata hai
+
+//primitive-->call by valur  7types:string,number,boolean,null,undefined,symbol,bigint
+
+//reference type/non-primitive-->
+//array,objects,functions  baki jitne bhi hai wo sare primitive me chle gye
+
+const score=100
+const scorevalue=100.3
+const isloggedin=false
+const outsidetemp=null
+let useremail; //undefined
+
+const id=symbol('123')
+const anotherid=symbol('123')
+
+console.log(id== anotherid) //false'
+
+const bignumber = 3475879034483984n
+
+
+ const heros= ["shaktiman","naagraj","daga"]
+
+ let myobj=
+ {
+    name:"adi",
+    age:22,
+
+ }
+
+ const myfunction=function(){
+    console.log("helloworld")
+ }
+
+
+console.log(typeof bignumber)
+
+//type of null->object,undefined->undefined
+
+
+
+
+ 
+
